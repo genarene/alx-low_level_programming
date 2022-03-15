6146f455804662f3840f9abbe5635d9c4cb9f7af
genarene <genarene96@gmail.com>
2022-03-15 09:59:20 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - program to print alphabet in lowercase
+ *   * Return: 0
+ *    */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+_putchar(c);
+_putchar('\n');
+}
