08dc4ed191942885f148439eb33b246f59bdbb4d
genarene <genarene96@gmail.com>
2022-03-15 10:53:44 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function that computes absolute value
+ * @n: string to print
+ * Return: void
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+n = (n * -1);
+return (n);	
+}
+}
