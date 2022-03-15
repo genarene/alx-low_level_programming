a0f145beb94607cf2ab398ae9c1165d26e57d753
genarene <genarene96@gmail.com>
2022-03-15 11:19:18 -0700

    addc

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds integers a and b
+ * @a: First integer
+ * @b: Second integer
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
