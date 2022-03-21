10d279d2781e1598e763a8eada8c34671912c13b
genarene <genarene96@gmail.com>
2022-03-21 15:13:25 -0700

    100atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copies str src to str pointer to dest
+ * @src: source string
+ * @dest: destination string
+ *
+ * Return: void
+ */
+int _atoi(char *s)
+{
+	int p[];
+
+	for(*(src + p) != '\0' ; p++)
+	{
+		*(dest + p) = *(src + p);
+	}
+	return (dest);
+}
