d15792b4d64eff45702b9c662e878579d88fe4ee
genarene <genarene96@gmail.com>
2022-03-21 14:54:56 -0700

    putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of string str
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int p = 0, a;
+
+	while (*(str + p) != '\0')
+	{
+		p++;
+	}
+	for (a = 0 ; a < p ; a++)
+	{
+		if (a > (p - 1) / 2)
+			_putchar(*(str + a));
+	}
+	_putchar('\n');
+}
