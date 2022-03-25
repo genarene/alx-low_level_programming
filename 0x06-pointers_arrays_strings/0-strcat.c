782a54d9b3cd6adb71dca48c5ee1ae23c05722fb
genarene <genarene96@gmail.com>
2022-03-25 12:45:18 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+	*s
+	f = _strchr(s, 'l');
+	printf("%s\n", f);
+	return (0);
+}
